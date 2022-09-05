be5d39ac1786c75a41121d6e8f17a94010038980
niklas <niklasi@mail.uni-paderborn.de>
2022-09-05 07:10:17 -0400

    java fix

--- install_java8.sh
+++ install_java8.sh
@@ -1,4 +1,3 @@
-wget http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-i586.tar.gzsudo
-sudo mkdir /usr/lib/jvm
-cd /usr/lib/jvm
-sudo tar -xvzf ~/Downloads/jdk-8u151-linux-x64.tar.gz
+wget http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-i586.tar.gz
+sudo cd /usr/lib/jvm
+sudo tar -xvzf ~/home/smcql_/jdk-8u151-linux-x64.tar.gz
