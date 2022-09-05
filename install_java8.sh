31b0944320fb086e06278bbfb7c58f6f401f1fcc
niklas <niklasi@mail.uni-paderborn.de>
2022-09-05 06:58:46 -0400

    java

---
+++ install_java8.sh
@@ -0,0 +1,4 @@
+wget http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-i586.tar.gzsudo
+sudo mkdir /usr/lib/jvm
+cd /usr/lib/jvm
+sudo tar -xvzf ~/Downloads/jdk-8u151-linux-x64.tar.gz
