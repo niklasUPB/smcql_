ba8f2ccbd6673197f14e042ece1228fee3f7a685
niklas <niklasi@mail.uni-paderborn.de>
2022-09-07 03:11:10 -0400

    123

--- execute.sh
+++ execute.sh
@@ -1,2 +1,2 @@
 #./build_and_execute.sh conf/workload/sql/comorbidity.sql testDB1 testDB2 
-./build_and_execute.sh conf/workload/sql/ez.sql testDB1 testDB2 
+./build_and_execute.sh conf/workload/sql/reverse.sql testDB1 testDB2 
