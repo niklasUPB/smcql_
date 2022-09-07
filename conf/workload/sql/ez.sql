42eb9b33393eeda136fe9c3c6964870eea9d19fe
niklas <niklasi@mail.uni-paderborn.de>
2022-09-07 02:52:30 -0400

    12

---
+++ conf/workload/sql/ez.sql
@@ -0,0 +1,1 @@
+SELECT t.take_id, m.me_id, FROM take t JOIN  me m  ON t.take_id = m.me_id 
--- execute.sh
+++ execute.sh
@@ -1,2 +1,2 @@
 #./build_and_execute.sh conf/workload/sql/comorbidity.sql testDB1 testDB2 
-./build_and_execute.sh conf/workload/sql/join.sql testDB1 testDB2 
+./build_and_execute.sh conf/workload/sql/ez.sql testDB1 testDB2 
