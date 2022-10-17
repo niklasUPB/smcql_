9c4f57d72b3053f06017ab94ba800a250afe412e
niklas <105489239+niklasUPB@users.noreply.github.com>
2022-10-17 03:43:47 +0200

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,2 +1,2 @@
 #./build_and_execute.sh conf/workload/sql/comorbidity.sql testDB1 testDB2 
-./build_and_execute.sh conf/workload/sql/use_case1.sql.sql testDB1 testDB2 
+./build_and_execute.sh conf/workload/sql/use_case1.sql testDB1 testDB2 
